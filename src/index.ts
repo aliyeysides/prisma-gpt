e902201c46d02cd268b25bcb83002911a0594506
Ali Yeysides <ali.yeysides@gmail.com>
2023-06-25 16:48:01 -0400

    forgot to add

--- src/index.ts
+++ src/index.ts
@@ -7,39 +7,50 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
-const queryFn = async (str: string) => {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "system", content: str }],
-  })
-  console.log('queryFn:', response.data.choices[0].message)
-  return response.data.choices[0].message
-}
-
 type Args = {}
-
-export const existsFn = (_extensionArgs: Args) =>
-  Prisma.defineExtension({
-    name: "prisma-extension-find-or-create",
-    model: {
-      $allModels: {
-        async exists<T, A>(
-          this: T,
-          args: Prisma.Exact<A, Prisma.Args<T, 'findFirst'>>
-        ): Promise<boolean> {
-
-          const ctx = Prisma.getExtensionContext(this)
-          const result = await (ctx as any).findFirst(args)
-          return result !== null
-        },
-      },
-    },
-  })
 
 export const queryGPT = (_extensionArgs: Args) =>
   Prisma.defineExtension({
     name: "prisma-gpt",
     client: {
-      $queryGPT: queryFn,
+      async $queryGPT(str: string, schema: string) {
+        const prompt = `
+          You are an AI assistant that returns raw sql queries using natural language. 
+          You only output raw SQL queries. Never return anything other than raw SQL.
+          Always begin the query with SELECT. You will be given the following schema:
+           ${schema}
+          Take the below query and return raw SQL (Postgres)):
+           ${str}
+          `
+        const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "system", content: prompt }],
+        })
+
+        if (!response.data.choices) {
+          throw new Error("No response from OpenAI")
+        }
+
+        const query = response.data.choices[0].message?.content
+        console.log('query:', response.data.choices[0])
+
+        if (!query) {
+          throw new Error("No query from OpenAI")
+        }
+
+        const newLinesAndCarriageReturns = /[\r\n]+/gm
+        const trimmed = query.replace(newLinesAndCarriageReturns, "")
+        console.log('query trimmed:', trimmed)
+
+        const ctx = Prisma.getExtensionContext(this)
+
+        try {
+          const res = await (ctx as any).$queryRawUnsafe(`${trimmed}`)
+          return res
+        } catch (e) {
+          console.log(e)
+        }
+      },
     },
   })
+
